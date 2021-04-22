bcf1662afca0c54ae56d9983941828a069779eb5
María <mariaocampo36@gmail.com>
2021-04-22 15:24:30 +0200

    Integración con Google Maps

--- FasterWho/app/src/main/java/com/muei/apm/fasterwho/IniciarRutaActivity.kt
+++ FasterWho/app/src/main/java/com/muei/apm/fasterwho/IniciarRutaActivity.kt
@@ -6,11 +6,22 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.RatingBar
 import android.widget.Toast
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class IniciarRutaActivity : AppCompatActivity() {
+class IniciarRutaActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var mMap: GoogleMap
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_iniciar_ruta)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.mapa_ruta) as SupportMapFragment
+        mapFragment.getMapAsync(this)
 
         val btnIniciarRuta : Button = findViewById(R.id.iniciar_ruta_button)
         btnIniciarRuta.setOnClickListener {
@@ -24,5 +35,14 @@ class IniciarRutaActivity : AppCompatActivity() {
             Toast.makeText(this, "Buscando Ubicación", Toast.LENGTH_SHORT).show()
         }
 
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
--- FasterWho/app/src/main/java/com/muei/apm/fasterwho/SeguimientoActivity.kt
+++ FasterWho/app/src/main/java/com/muei/apm/fasterwho/SeguimientoActivity.kt
@@ -5,11 +5,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class SeguimientoActivity : AppCompatActivity() {
+class SeguimientoActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_seguimiento)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
 
         val btnFinalizarRuta : Button = findViewById(R.id.finalizar_ruta_button)
         btnFinalizarRuta.setOnClickListener {
@@ -17,5 +30,14 @@ class SeguimientoActivity : AppCompatActivity() {
             val intent = Intent(this, PopUpSaveRoute::class.java)
             startActivity(intent)
         }
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
